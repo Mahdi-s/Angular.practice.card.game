a20fc86e0466d635eff954aead75992b1413263d
Mahdi-s <mahdi.sv98@gmail.com>
2024-08-17 11:25:44 -0500

    front end draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { GameComponent } from './components/game/game.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [GameComponent]
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Memory Game';
 }
